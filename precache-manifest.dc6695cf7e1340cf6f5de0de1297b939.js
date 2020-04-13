679495f1d8a60507ba081a51b12eb3278166012a
Victor Arnaud <victorhad@gmail.com>
2020-04-13 20:56:16 -0300

    Updates

---
+++ precache-manifest.dc6695cf7e1340cf6f5de0de1297b939.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a08c0984f22c4aeeb7700409541ce6a5",
+    "url": "/vwapp-react-form/index.html"
+  },
+  {
+    "revision": "3ad4a940f3f909cd067b",
+    "url": "/vwapp-react-form/static/css/2.5f71d73f.chunk.css"
+  },
+  {
+    "revision": "3ad4a940f3f909cd067b",
+    "url": "/vwapp-react-form/static/js/2.a5c11007.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/vwapp-react-form/static/js/2.a5c11007.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a92935e3f1439c1c78ab",
+    "url": "/vwapp-react-form/static/js/main.d7cf90cc.chunk.js"
+  },
+  {
+    "revision": "478eb89126d9ad6ad869",
+    "url": "/vwapp-react-form/static/js/runtime-main.4d50da00.js"
+  },
+  {
+    "revision": "6102afa3a36a1b04edb04d17e64a3f1e",
+    "url": "/vwapp-react-form/static/media/user~bWEoNccF.6102afa3.png"
+  }
+]);
